da91b3dbec73038091ce297b4d1c1aefc1e42666
krishnasarathi <krishnasarathishem@gmail.com>
2025-03-14 22:18:54 +0530

    create get_meme_templates function to retrieve templates from imgflip

--- main.py
+++ main.py
@@ -3,3 +3,12 @@ import ollama
 from PIL import Image, ImageDraw, ImageFont
 from io import BytesIO
 import difflib
+
+def get_meme_templates():
+    url = "https://api.imgflip.com/get_memes"
+    response = requests.get(url).json()
+    if response["success"]:
+        return response["data"]["memes"]
+    return []
+
+templates = get_meme_templates()
